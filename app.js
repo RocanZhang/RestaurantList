915a298d7ff1bef67f155d4eb85e8ad08aa5e7a6
Rocan87447 <rocan87447@gmail.com>
2024-01-10 23:52:00 +0800

    01/10 build: 1. add static files & json for model 2. use express-handlebars as view engine 3. add bootstrap static files for style

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+
+//設定 Express 的 view engine 是用 .hbs (代替handlebars)
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("express app for RestaurantList");
