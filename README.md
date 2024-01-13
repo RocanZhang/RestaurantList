c21526661189dac7aec6fc6916c22c329ef34ef0
Rocan87447 <rocan87447@gmail.com>
2024-01-13 22:25:31 +0800

    01/13 build: 1. List data + details + search function 2. Screenshot 3. README.md Writing

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
-# 餐廳清單
+# 精選餐廳清單
+
+![image](./public/screenshot/screenshort1.png)
+![image](./public/screenshot/screenshort2.png)
+![image](./public/screenshot/screenshort3.png)
 
 ## 簡介
 
+提供精選的優質餐廳，可用搜尋功能快速查找，並完整提供餐廳資訊。
+
 ### 功能
+
+1. 查看所有精選餐廳
+2. 快速搜尋餐廳
+3. 瀏覽餐廳的詳細資訊
+4. 連結餐廳的地址到 Google 地圖
 
 ## 使用步驟
 
-
-5. 步驟五:若看見此行訊息則代表順利運行，打開瀏覽器進入到以下網址
-
+1. 請先確認有安裝 node.js 與 npm
+2. 開啟終端機工具: Git Bash
+3. 將專案 clone 到本地，請輸入
+   ```
+   git clone https://github.com/RocanZhang/RestaurantList.git
+   ```
+4. 在本地開啟之後，透過終端機進入資料夾，輸入：
+   ```bash
+   npm install
+   ```
+5. 安裝完畢後，執行專案請輸入：
+   ```bash
+   npm run start
+   ```
+6. 若看見此行訊息則代表順利運行，打開瀏覽器進入到以下網址
    ```bash
    Listening on http://localhost:3000
+   ```
+7. 停止連線狀態，請輸入
+   ```bash
+   ctrl + c
    ```
 
 ## 開發工具
--- app.js
+++ app.js
@@ -2,15 +2,59 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const restaurants = require("./public/jsons/restaurant.json").results;
 
 //設定 Express 的 view engine 是用 .hbs (代替handlebars)
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("express app for RestaurantList");
+  res.redirect("/search");
+  // res.render("index", { restaurants })
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword?.trim();
+  const matchedRestaurants = keyword
+    ? restaurants.filter((res) =>
+        Object.values(res).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          } else {
+            return false;
+          }
+        })
+      )
+    : restaurants;
+  res.render("index", { restaurants: matchedRestaurants, keyword });
+});
+
+// app.get("/search", (req, res) => {
+//   if (!req.query.keywords) {
+//     return res.redirect("/");
+//   }
+
+//   const keywords = req.query.keywords;
+//   const keyword = req.query.keywords.trim().toLowerCase();
+
+//   const filterrestaurants = restaurants.filter(
+//     (data) =>
+//       data.name.toLowerCase().includes(keyword) ||
+//       data.category.includes(keyword)
+//   );
+
+//   res.render("index", { restaurants: filterrestaurants, keywords });
+// });
+
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  const restaurant = restaurants.find(
+    (restaurant) => restaurant.id.toString() === id
+  );
+  res.render("detail", { restaurant });
 });
 
 app.listen(port, () => {
